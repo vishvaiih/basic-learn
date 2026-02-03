5dd409f6d98f02b40ed767f35a669c7f71c16362
vishvaiih <vishva.iihglobal@gmail.com>
2026-02-03 15:40:41 +0530

    some changes

--- basic-learn/src/Components/LeftSide.jsx
+++ basic-learn/src/Components/LeftSide.jsx
@@ -1,20 +1,20 @@
 import { Box, Button, styled, TextField, Typography } from "@mui/material";
-import React from "react";
+import React ,{useState} from "react";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
-
+import SelectOption from "./SelectOption";
 
 export const MainBox = styled(Box)({
   width: "60%",
   border: "1px solid #d1d8be",
   maxHeight: "60vh",
   borderRadius: "15px",
-  overflowY:"auto",
-  overflowX:"hidden",
-//   marginTop: "4%",
+  overflowY: "auto",
+  overflowX: "hidden",
+  //   marginTop: "4%",
   padding: "2%",
 });
 
- export const AddTask = styled(Box)({
+export const AddTask = styled(Box)({
   display: "flex",
   alignItems: "center",
 });
@@ -34,20 +34,30 @@ const Field = styled(TextField)({
   "& .MuiOutlinedInput-root": {
     height: "32px",
     borderRadius: "10px",
-    '&:hover fieldset': {
-      borderColor: '#7d3bed', 
-    }
+    "&:hover fieldset": {
+      borderColor: "#7d3bed",
+    },
   },
 });
 
-function LeftSide({handleChange,handleAdd,task}) {
+function LeftSide({ handleChange, handleAdd, task }) {
+
+  const [priority, setPriority] = useState("Medium Priority");
+
+  const handlePriorityChange = (event) => {
+    console.log("....",event.target.value);
+    setPriority(event.target.value);
+  };
+
+  const optionOfPriority = ["Low Priority", "Medium Priority", "High Priority"];
   return (
-    <MainBox sx={{margin:"3% 3% 0% 0%"}}>
+    <MainBox sx={{ margin: "3% 3% 0% 0%" }}>
       <AddTask>
-        <AddOutlinedIcon sx={{ fontSize: "12px", marginRight: "2%",color:"#7d3bed" }} />
+        <AddOutlinedIcon
+          sx={{ fontSize: "12px", marginRight: "2%", color: "#7d3bed" }}
+        />
         <Typography sx={{ fontSize: "12px" }}>Add a task</Typography>
       </AddTask>
-
 
       <Field
         onChange={(e) => handleChange(e)}
@@ -59,11 +69,11 @@ function LeftSide({handleChange,handleAdd,task}) {
         InputProps={{
           style: {
             fontSize: 11,
-           
           },
         }}
       />
-      
+
+      <SelectOption priority={priority} setPriority={setPriority} handlePriorityChange={handlePriorityChange} optionOfPriority={optionOfPriority}/>
       <Btn onClick={handleAdd}>Add Task</Btn>
     </MainBox>
   );
--- basic-learn/src/Components/SelectOption.jsx
+++ basic-learn/src/Components/SelectOption.jsx
@@ -1,19 +1,66 @@
-import React from 'react'
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import React, { useState } from "react";
+import { Box, styled } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-function SelectOption() {
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { Check } from "lucide-react";
+
+const Form = styled(FormControl)({
+  marginTop: "4%",
+  "& .MuiOutlinedInput-root": {
+    height: "32px",
+    borderRadius: "10px",
+    "&:hover fieldset": {
+      borderColor: "#7d3bed",
+    },
+  },
+});
+
+const Item = styled(MenuItem)({
+  margin: "2%",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  "&.Mui-selected": {
+    backgroundColor: "#9929ea",
+    borderRadius: "8px",
+    color: "#4d2fb2",
+    "&:hover": {
+      backgroundColor: "none",
+    },
+  },
+});
+
+function SelectOption({priority,handlePriorityChange,optionOfPriority}) {
+ 
+
   return (
     <>
-
-    
-        
-    
+      <Form fullWidth>
+        <Select
+          value={priority}
+          onChange={handlePriorityChange}
+          sx={{
+            fontSize: "13px",
+            // color: '#b9b9b9',
+          }}
+        >
+          {optionOfPriority.map((itm) => (
+            <Item value={itm}>
+              {itm}
+              {/* {priority === itm && (
+                <Box>
+                  <Check />
+                </Box>
+              )} */}
+            </Item>
+          ))}
+        </Select>
+      </Form>
     </>
-  )
+  );
 }
 
-export default SelectOption
+export default SelectOption;
